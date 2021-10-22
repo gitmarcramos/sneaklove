5450c45bba1d1dc076c7858d99c356db6fbcf152
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-10-22 11:01:55 +0200

    CRUD done

--- routes/dashboard-Sneaker.routes.js
+++ routes/dashboard-Sneaker.routes.js
@@ -1,9 +1,11 @@
 const express = require("express"); // import express in this module
 const router = express.Router(); // create an app sub-module (router)
 const sneakerSchema = require("./../models/Sneaker");
-const protectPrivateRoute = require("../middlewares/protectPrivateRoute")
+const protectPrivateRoute = require("../middlewares/protectPrivateRoute");
 
-router.use(protectPrivateRoute);
+// SET the DASHBOARD router to use AUTH middleware
+// router.use(protectPrivateRoute);
+
 //GET dashboard view
 router.get("/", async (req, res) => {
   try {
@@ -30,15 +32,41 @@ router.post("/products-add", async (req, res, next) => {
   }
 });
 
-// GET sneaker ID
+// GET sneaker ID and delete item
 router.get("/delete/:id", async (req, res, next) => {
   try {
-      console.log(req.params.id)
-      await sneakerSchema.findByIdAndDelete(req.params.id);
-      res.redirect("/dashboard")
+    console.log(req.params.id);
+    await sneakerSchema.findByIdAndDelete(req.params.id);
+    res.redirect("/dashboard");
   } catch (err) {
     console.log(err);
   }
 });
 
+// GET sneakers ID and update
+router.get("/product-edit/:id", async (req, res) => {
+  try {
+    const sneaker = await sneakerSchema.findById(req.params.id);
+    res.render("product-edit.hbs", { sneaker });
+  } catch (err) {
+    console.log(err, "Canno't GET Product Edit route");
+  }
+});
+
+// POST sneakers ID and update
+router.post("/product-edit/:id", async (req, res) => {
+  try {
+    const sneaker = await sneakerSchema.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+
+    console.log("==========>", req.body)
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.log(err, "Canno't POST Product Edit route");
+  }
+});
+
 module.exports = router;
+
